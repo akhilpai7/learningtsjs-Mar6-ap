45db38d478caa9d721c989f3ca2de2a9b2c61148
akhilpai7 <48257934+akhilpai7@users.noreply.github.com>
2019-03-06 12:41:05 -0700

    demo spec

---
+++ spec/00_demo_spec.ts
@@ -0,0 +1,7 @@
+describe('writing basic specs', () => {
+    it('we can add two numbers together', () => {
+        const a = 10, b = 20;
+        const answer = a + b;
+        expect(answer).toBe(30);
+    });
+});
